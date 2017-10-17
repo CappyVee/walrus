2e397fcf74040888e95a88d1e4cd97b8425aa2b1
Chris Quattrocchi and Zachary Marston <zachary@>
2017-10-17 10:32:24 -0700

    add background color change and text color change functionality

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,16 @@ $(document).ready(function() {
     $(".aardvark-showing").toggle();
     $(".aardvark-hidden").toggle();
   });
+  $(".bgdark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+    $(".dark-hidden").toggle();
+    $(".dark-showing").toggle();
+  });
+  $(".bgwhite").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+    $(".dark-hidden").toggle();
+    $(".dark-showing").toggle();
+  });
 });
